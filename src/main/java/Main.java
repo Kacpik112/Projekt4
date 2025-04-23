8b076b881f2e90429675f69e53647252a7c8e2f4
Kacpik112 <66215363+Kacpik112@users.noreply.github.com>
2025-04-23 12:48:26 +0000

    commit_4_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,33 @@ Kod bazowy programu Commit4_0:
 */
  
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Podaj imię studenta: ");
+      String name = scanner.nextLine();
+
+      System.out.print("Podaj wiek studenta: ");
+      int age = scanner.nextInt();
+
+      s.addStudent(new Student(name, age));
+      System.out.println("Student dodany!");
 
       var students = s.getStudents();
+      System.out.println("\nLista studentów:");
       for(Student current : students) {
         System.out.println(current.ToString());
       }
-    } catch (IOException e) {
 
+      scanner.close();
+
+    } catch (IOException e) {
+      System.out.println("Błąd: " + e.getMessage());
     }
   }
 }
